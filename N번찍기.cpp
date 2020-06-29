57e4c3cd692cd9b1697b297cf038e5ea15107df2
hmc1206 <67060344+hmc1206@users.noreply.github.com>
2020-06-29 13:25:12 +0900

    Add files via upload

---
+++ N번찍기.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+	int num=0,i,n;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		num+=1;
+		printf("%d\n",&num);
+	}
+	return 0;
+}
---
+++ n번 기찍.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+	int num,i;
+	scanf("%d",&num);
+	for(i=num;i>0;i--){
+		printf("%d\n",i);
+	}
+	return 0;
+} 
